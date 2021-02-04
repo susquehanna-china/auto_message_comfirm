256c7382ace647cbf82b466f2d6a3f64d57d5210
ronnie <ronnie.yang@sig.com>
2021-02-04 10:34:37 +0800

    first commit

--- auto_message/send_message.py
+++ auto_message/send_message.py
@@ -14,11 +14,14 @@ def get_data():
 
 
 judge = 0
+data = []
 while True:
     if time.localtime().tm_sec == 10 and judge == 0:
         judge = 1
         data = get_data()
         print(data)
+        # send message
         time.sleep(2)
         judge = 0
+
 
